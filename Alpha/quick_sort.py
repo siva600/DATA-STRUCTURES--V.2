6ac10973aed5a38302ce701e6ad411640b8a87ce
Siva. V <vsivachaitanya60@gmail.com>
2020-02-18 01:06:12 +0000

    added quick sort

---
+++ Alpha/quick_sort.py
@@ -0,0 +1,26 @@
+# using merge sort for a given list of items
+# steps : 
+# 1. pick a pivot
+# 2. move the items less than pivot to left side
+# 3. move the items greater than pivot to right side
+# 4. if the result is not sorted, follow the process recursively for # left items and right items
+
+def quick_sort(sequence):
+  length = len(sequence)
+  if len(sequence) <= 1:
+    return sequence
+  else:
+    pivot = sequence.pop()  # here picking last item as the pivot.
+  
+  items_lower = []
+  items_greater = []
+  for item in sequence:
+    if item > pivot:
+      items_greater.append(item)
+    else:
+      items_lower.append(item)
+  
+  return quick_sort(items_lower) + [pivot] + quick_sort(items_greater)
+
+sequence = [4,5,10,0,1,2,8, 11, 20,20,20, 25]
+print(quick_sort(sequence))
