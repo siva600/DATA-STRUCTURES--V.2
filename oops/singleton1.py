69bb4cc97908bc2421cd9b837588b5890c255354
Siva. V <vsivachaitanya60@gmail.com>
2020-03-05 10:13:37 +0000

    singleton

--- oops/singleton1.py
+++ oops/singleton1.py
@@ -1,5 +1,6 @@
 # single object creation
 # https://www.youtube.com/watch?v=6IV_FYx6MQA
+# singleton using self.
 class SingeleTon(object):
     _instance = None
 
@@ -11,6 +12,7 @@ class SingeleTon(object):
 
 
 # https://www.youtube.com/watch?v=wElVjMlYVAw
+# singleton using cls instance.
 class SingleTon2(object):
     # new method is basically used ofr creating an object. This is called for object creation and instance.
     def __new__(cls, *args, **kwargs):
@@ -33,4 +35,31 @@ print obj2.y
 
 print(id(obj))
 print(id(obj2))
+
+
+# ************************************
+# Create a singleton using a decorator.
+def singleton3(myClass):
+    instances = {}  # create a instance dict to store instances.
+
+    # passing the args and kwargs to accept any items in myClass
+    def create_instance(*args, **kwargs):
+        if myClass not in instances:
+            # if the instance is not in out dict assign a new instance here.
+            instances[myClass] = myClass(*args, **kwargs)
+        return instances[myClass]
+    return create_instance
+
+# assign the decorator to the destiny singleton class
+@singleton3
+class TestClass(object):
+    pass
+
+
+x = TestClass()
+x.data = 10
+print x.data
+
+y = TestClass()
+print y.data
 
