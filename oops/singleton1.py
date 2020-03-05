d9e9e526c6d1d41178ed09371a8c025474b0f09a
Siva. V <vsivachaitanya60@gmail.com>
2020-03-05 09:53:18 +0000

    test

--- oops/singleton1.py
+++ oops/singleton1.py
@@ -10,6 +10,19 @@ class SingeleTon(object):
         return self._instance
 
 
+# https://www.youtube.com/watch?v=wElVjMlYVAw
+class SingleTon2(object):
+    # new method is basically used ofr creating an object. This is called for object creation and instance.
+    def __new__(cls, *args, **kwargs):
+        # check is there any attribute available ..
+        if not hasattr(cls, '_instance'):
+            # create the actual object using super keywoed. and stored in cls attribute instance
+            cls._instance = super(SingleTon2, cls).__new__(cls)
+        # if it already has the _instance attribute it will return the existing object
+        return cls._instance
+
+
+
 obj = SingeleTon()
 print obj.y
 obj.y = 20
