4fbc769f3930a20ec2e1d56ecf6daa98610713cb
Siva. V <vsivachaitanya60@gmail.com>
2020-03-08 07:34:12 +0000

    added file

---
+++ oops/dunder_functions.py
@@ -0,0 +1,30 @@
+class Countries:
+    def __init__(self):
+        self._members = ['India', 'America', 'England', 'Russia', 'China']
+
+    def __len__(self):
+        return len(self._members)
+
+    def __contains__(self, item):
+        return item in self._members
+
+    def __getitem__(self, item):
+        if isinstance(item, int):
+            return self._members.pop(item)
+
+    def __iter__(self):
+        while self._members:
+            yield self._members.pop()
+
+
+union = Countries()  # initialize the object
+
+print "Get 1st item:", union[0]     # use get item to fetch an item from the created object.
+
+for item in union:  # use union as iterator to fetch one by one using generator.
+    print "generated item:", item
+
+print "length: ", len(union)   # finally get length of items. This would return 0.
+
+union2 = Countries()
+print "length for 2nd object: ", len(union2)
