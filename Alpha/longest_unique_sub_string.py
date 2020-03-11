f4b1077cc9c59188ca2df2e12a5e3a323b6dbd74
Siva. V <vsivachaitanya60@gmail.com>
2020-03-11 03:55:55 +0000

    added files

---
+++ Alpha/longest_unique_sub_string.py
@@ -0,0 +1,44 @@
+# given a string find the LONGEST SUBSTRING WITHOUT REPEATS.
+# e.g.. 'abcabcbb' == 3
+
+
+def helper(s, start):
+    seen = set()
+    for i in range(start, len(s)):
+        if s[i] not in seen:
+            seen.add(s[i])
+        else:
+            return i-start
+    return len(s) - start
+
+
+def max_string(s):
+    max_length = 0
+    for i in range(len(s)):
+        max_length = max(max_length, helper(s, i))
+        if max_length == len(s[i:]) or max_length > len(s[i:]):
+            return max_length
+    return max_length
+
+
+# sliding window approach.
+# initiate start, end, max_length.
+def max_substring(s):
+    seen = {}
+    start = 0
+    max_length = 0
+    for i in range(len(s)):
+        if s[i] not in seen:
+            seen[s[i]] = i
+            if max_length <= len(s[start:i]) + 1:
+                max_length = len(s[start:i]) + 1
+        else:
+            start = seen[s[i]] + 1
+            seen[s[i]] = i
+
+    return max_length
+
+
+print(max_string("bbacbd"))
+print(max_substring("pwwkew"))
+
